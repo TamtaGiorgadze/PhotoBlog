e2c05be1835ff57b759ccf7b918313f1992ab017
TamtaGiorgadze <tamt.giorgadze@gmail.com>
2020-09-01 15:02:00 +0400

    add custum 404_page

--- main.go
+++ main.go
@@ -17,9 +17,16 @@ func contact(w http.ResponseWriter, r *http.Request)  {
 	fmt.Fprint(w, "<h1>Contact Page</h1>")
 }
 
+func notFound(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>Sorry, we couldn't find the page you were looking for</h1>")
+}
+
 func main() {
 	r := mux.NewRouter()
 
+	r.NotFoundHandler = http.HandlerFunc(notFound)
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 
