c6a92423670c760c172ffe4b183fc76e561fa5ec
TamtaGiorgadze <tamt.giorgadze@gmail.com>
2020-09-01 14:54:00 +0400

    add home and contact func

--- main.go
+++ main.go
@@ -7,23 +7,21 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request)  {
 	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Main Page</h1>")
+}
 
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Main Page</h1>")
-	} else if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "<h1>Contact Page</h1>")
-	} else {
-		fmt.Fprint(w, "<h1>Fuck Off!</h1>")
-	}
+func contact(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Contact Page</h1>")
 }
 
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", handlerFunc)
-	r.HandleFunc("/contact", handlerFunc)
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
 
 	http.ListenAndServe(":3000", r)
 }
