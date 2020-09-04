deb4a0512a857965809fdcfb587b5624218472de
TamtaGiorgadze <tamt.giorgadze@gmail.com>
2020-09-04 18:29:01 +0400

    created home and contact view

--- main.go
+++ main.go
@@ -8,21 +8,26 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var homeTemplate *template.Template
+var (
+	homeTemplate    *template.Template
+	contactTemplate *template.Template
+)
 
-func home(w http.ResponseWriter, r *http.Request)  {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	if err := homeTemplate.Execute(w, nil); err != nil {
 		panic(err)
 	}
 }
 
-func contact(w http.ResponseWriter, r *http.Request)  {
+func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Contact Page</h1>")
+	if err := contactTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
-func notFound(w http.ResponseWriter, r *http.Request)  {
+func notFound(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusNotFound)
 	fmt.Fprint(w, "<h1>Sorry, we couldn't find the page you were looking for</h1>")
@@ -31,6 +36,10 @@ func notFound(w http.ResponseWriter, r *http.Request)  {
 func main() {
 	var err error
 	homeTemplate, err = template.ParseFiles("views/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	contactTemplate, err = template.ParseFiles("views/contact.gohtml")
 	if err != nil {
 		panic(err)
 	}
